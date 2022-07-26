ec75bb56ac10e20f141b2c58377ecd4357e3561f
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2022-07-25 22:54:30 -0700

    tokens

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
 if__name__=="__main__":
 	print("hello ZA world")
 xapp-1-A03QZ9UFZR8-3861977259569-457d484720dc9865f6813ed42f248b055323a07d2f4aa58daeb1076d79627c7b
+
+bot token: xoxb-3842720262582-3846401217797-nvGJP8N7tAtipgBAIk5qz7Pk
